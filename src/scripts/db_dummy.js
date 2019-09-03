cb53f41757a4e6ca3e6616b22f2cfe7988d2e34c
josh <joshcarr132@gmail.com>
2019-09-03 11:00:17 -0400

    added script to populate db with dummy data (for testing)

---
+++ src/scripts/db_dummy.js
@@ -0,0 +1,42 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'bci-dev';
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+// Use connect method to connect to the server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  insertDocuments(db, function() {
+    client.close();
+  });
+});
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('passwords');
+
+  // remove everything in the db
+  collection.deleteMany({});
+
+  // add document
+  collection.insertOne({
+    id: 99,
+    start: [2, 2],
+    commands: ['left', 'up', 'right', 'right'],
+    circle: [3, 1],
+    pathString: 'M450,450L270,450L270,270L450,270L630,270',
+  }, (err, result) => {
+    assert.equal(err, null);
+    console.log('Inserted document into the collection');
+    callback(result);
+  });
+};
