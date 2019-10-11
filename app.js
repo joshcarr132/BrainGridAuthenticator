9a03088f8ed72f858624b5b034f17f8fdcf1929f
josh <joshcarr132@gmail.com>
2019-10-11 10:15:41 -0400

    comment Auth import; not working with this for now

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let connection;
 
 
 const commandBlock = require('./src/scripts/commandBlock.js');
-const Auth = require('./src/scripts/auth.js');
+// const Auth = require('./src/scripts/auth.js');
 
 let ctxClient;
 
