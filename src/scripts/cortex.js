9c1b544925ff96f56eb50c0e247ec3e272f7c926
josh <joshcarr132@gmail.com>
2019-10-18 12:25:56 -0400

    implemented logging, building out class methods

--- src/scripts/cortex.js
+++ src/scripts/cortex.js
@@ -3,80 +3,168 @@ const CORTEX_URL = 'wss://localhost:6868';
 
 
 class Cortex {
-    constructor(options = {}) {
+  constructor(auth, options = {}) {
+        this.options = options;
+        this.auth = auth;
+
         process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
         this.ws = new WebSocket(CORTEX_URL);
 
         this.messageId = 0;
+        this.awaitingResponse = 0;
 
         this.ws.addEventListener('close', () => {
-            this._log('ws: Socket closed');
+            this.log('ws: Socket closed');
         });
 
         this.ready = new Promise((resolve) => {
             this.ws.addEventListener('open', resolve);
-        })
-
-        .then(() => console.log('ws: Socket opened'));
+        }).then(() => this.log('ws: Socket opened'));
     }
 
     call(method, params = {}) {
         const messageId = this.messageId++;
         const request = {
-            "jsonrpc": "2.0",
-            "method": method,
-            "params": params,
-            "id": messageId,
+            jsonrpc: '2.0',
+            method: method,
+            params: params,
+            id: messageId,
         };
 
         return new Promise((resolve, reject) => {
-
-            this.ws.send(JSON.stringify(request));
+            const message = JSON.stringify(request);
+            this.ws.send(message);
+            this.log(`ws: sending[${messageId}: ${method}] ${message}`);
+            this.awaitingResponse++;
 
             this.ws.on('message', (data) => {
                 const response = JSON.parse(data);
                 if (response['id'] === messageId) {
+                    this.log(`ws: received[${messageId}: ${method}] ${JSON.stringify(response)}`);
+                    this.awaitingResponse--; 
                     resolve(response['result']);
                 }
             });
         });
     }
 
-    authorize(auth) {
-        const params = {
-            clientId: auth.client_id,
-            clientSecret: auth.client_secret,
-            debit: 1,
-        };
+    authorize(auth = this.auth) {
+        return new Promise((resolve, reject) => {
+            const params = {
+                clientId: auth.client_id,
+                clientSecret: auth.client_secret,
+                debit: 1,
+            };
 
-        this.call("authorize", params)
-            .then((result) => {
-                this.authToken = result.cortexToken;
-            });
+            this.call('authorize', params)
+                .then((result) => {
+                    this.authToken = result.cortexToken;
+                    this.log(`ctx: assigning authToken: ${result.cortexToken}`);
+                    resolve(result.cortexToken);
+                });
+        });
+    }
+
+    getHeadsetId() {
+        return new Promise((resolve, reject) => {
+            this.call('queryHeadsets')
+                .then((result) => {
+                    const hsId = result[0].id;
+                    this.headsetId = hsId;
+                    this.log(`ctx: assigning headsetId: ${hsId}`);
+                    resolve(hsId);
+                });
+        });
+    }
+
+    initSession(token = this.authToken) {
+        this.log('ctx: initializing session');
+
+        return new Promise((resolve, reject) => {
+            if (this.authToken) {
+                this.authorize(this.auth)
+                    .then((token) => {
+                        this.initSession(token);
+                        reject();
+                    });
+            }
+
+            const params = {
+                cortexToken: this.authToken,
+                status: 'open',
+                headset: this.headsetId,
+            };
+
+            this.call('createSession', params)
+                .then((result) => {
+                    //
+                    this.session = result;
+                    resolve(result);
+                });
+        });
+    }
+
+    closeSession() {
+        this.log('ctx: closing session');
+        //
+        return new Promise((resolve, reject) => {
+
+        });
+ 
+    }
+
+
+    log(...msg) {
+        if (this.options.verbose == true) {
+            console.log('-----');
+            console.log(...msg);
+        }
+    }
+
+    close(force = false) {
+        if (force) {
+            this.ws.close();
+        } else if (this.awaitingResponse == 0) {
+            this.ws.close();
+        } else {
+            setTimeout(() => { this.close(); }, 250);
+        }
     }
 }
 
-
 // test methods here
-const auth = require("./auth.js");
-const ctx = new Cortex();
+const auth = require('./auth.js');
+const ctx = new Cortex(auth, {verbose: true});
+
+// ctx.ready.then(() => {
+//     ctx.call('getCortexInfo')
+//           .then((result) => {
+//               // console.log(result);
+//           });
+// }).then(() => {
+//     ctx.call('getUserLogin')
+//         .then((result) => {
+//             // console.log(result);
+//         });
+// }).then(() => {
+//     ctx.getHeadsetId()
+//         .then(() => {
+//             ctx.authorize(auth)
+//                 .then(() => {
+//                     ctx.initSession();
+//                 });
+//         });
+// })
+//     .then(() => {
+//     ctx.close();
+// });
 
 ctx.ready.then(() => {
-    ctx.call("getCortexInfo")
-          .then((result) => {
-              console.log(result);
-          });
-}).then(() => {
-    ctx.call("getUserLogin")
-        .then((result) => {
-            console.log(result);
+    ctx.authorize().then(() => {
+        ctx.getHeadsetId().then(() => {
+            ctx.initSession();
         });
-}).then(() => {
-    ctx.authorize(auth);
-}).then(() => {
-    ctx.call("queryHeadsets")
-        .then((result) => {
-            console.log(result);
-        });
+    });
 });
+
 
