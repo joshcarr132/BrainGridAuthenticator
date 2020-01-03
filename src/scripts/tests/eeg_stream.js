fcb0aba663b67f0b26afeda8f47caee103bf4dcb
josh <joshcarr132@gmail.com>
2020-01-03 13:49:34 -0500

    debugging

--- src/scripts/tests/eeg_stream.js
+++ src/scripts/tests/eeg_stream.js
@@ -6,7 +6,8 @@ const ctx = new Cortex(Auth, { verbose: true });
 ctx.ready.then(() => {
   ctx.authorize().then(() => {
     ctx.createSession({ auth: ctx.auth, status: 'open'})
-      .then(() => {
+      .then((result) => {
+        console.log(result);
         ctx.subscribe(['eeg']).then((subs) => {
           ctx.ws.on('message', (msg) => {
             console.log(msg);
